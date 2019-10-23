44f764fab62bd45fd095362e3b10cfbb0eef63f3
ujjwal <ujjwal@infotrack115.citcert.com>
2019-10-23 16:16:43 +0530

    Change in test base

--- src/main/java/com/test/automation/TestBase/BaseClass.java
+++ src/main/java/com/test/automation/TestBase/BaseClass.java
@@ -179,7 +179,7 @@ public class BaseClass
 		Assert.assertEquals(actual_msg, expected_msg);
 	}
 	
-	//Test         
+	//Test rteter        
 	@BeforeTest
 	public void Extent_Report_Generation() throws IOException
 	{
